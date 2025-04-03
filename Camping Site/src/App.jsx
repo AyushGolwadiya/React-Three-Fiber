78c62d264d5ba0a970155de69782d70ecf5433f3
AyushGolwadiya <ayushgolvadiya@gmail.com>
2025-04-03 20:12:27 +0530

    Camping Site

---
+++ Camping Site/src/App.jsx
@@ -0,0 +1,26 @@
+import {Suspense} from 'react'
+import Experience from './components/Experience'
+import {Canvas} from '@react-three/fiber'
+import { Bloom, EffectComposer } from '@react-three/postprocessing'
+import UI from './components/UI'
+
+const App = () => {
+  return (
+    <>
+    <Canvas shadows camera={{position:[0,0,8],fov:42}}>
+      <color attach="background"  args={["#111"]}/>
+      <fog attach="fog" args={["#171720",10,30]}/>
+      <Suspense>
+        <Experience /> 
+      </Suspense>
+      <EffectComposer>
+        <Bloom  mipmapBlur intensity={1}/>
+      </EffectComposer>
+    </Canvas>
+    <UI />
+    </>
+
+  )
+}
+
+export default App
